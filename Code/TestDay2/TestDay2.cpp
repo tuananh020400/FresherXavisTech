a19911d1d5519e56c98cf0e3180c90ad5d8f50d0
Tuan Anh <tuananh020400@gmail.com>
2025-05-13 13:28:03 +0700

    Add code

---
+++ Code/TestDay2/TestDay2.cpp
@@ -0,0 +1,182 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+void CalculateHistAndCDF(Mat& image, vector<float>& hist, vector<float>& cdf);
+Mat ManualHistogramEqualization(Mat& src);
+Mat ManualMachingHistogram(Mat& src, Mat& ref);
+Mat Display(Mat& image);
+Mat LocalHistogramEqualization(Mat& image, int blockSize);
+
+int main() {
+    // Read the image in grayscale
+    Mat image = imread("C:/Users/tuana/Downloads/Image1.png", IMREAD_GRAYSCALE);
+    Mat ref = imread("C:/Users/tuana/Downloads/Screenshot 2025-05-09 154617.png", IMREAD_GRAYSCALE);
+    if (image.empty() || ref.empty()) {
+        cout << "Could not open or find the image!\n";
+        return -1;
+    }
+    //Original Histogram
+    Mat imageHist = Display(image);
+    Mat refHist = Display(ref);
+
+    imshow("Image Histogram", imageHist);
+    imshow("Ref Histogram", refHist);
+
+    ////Manual histogram equalization
+    //Mat manualEqualized_img = image.clone();
+    //manualEqualized_img = ManualHistogramEqualization(image);
+    //Mat manualEqualizeHist = Display(manualEqualized_img);
+    //imshow("Manual Equalize Image", manualEqualized_img);
+    //imshow("Manual Equalize Histogram", manualEqualizeHist);
+
+    //// Perform histogram equalization using OpenCV
+    //Mat equalized_img = image.clone();
+    //equalizeHist(image, equalized_img);
+    //Mat EqualizeHist = Display(equalized_img);
+    //imshow("Equalize Histogram", EqualizeHist);
+
+    //// Manual histogram specification
+    //Mat manualMatching_img = ManualMachingHistogram(image, ref);
+    //imshow("Manual Matching Image", manualMatching_img);
+    //Mat manualMatchingHist = Display(manualMatching_img);
+    //imshow("Manual Matching Histogram", manualMatchingHist);
+
+    // Manual local histogram equalization
+    Mat localHistogram = LocalHistogramEqualization(image, 32);
+    imshow("Local Histogram Equalization", localHistogram);
+
+    waitKey(0);
+    return 0;
+}
+
+void CalculateHistAndCDF(Mat& image, vector<float>& hist, vector<float>& cdf)
+{
+    int total = image.rows * image.cols;
+
+    // Calculate Hist
+    hist.assign(256, 0);
+    for (int i = 0; i < image.rows; i++)
+    {
+        for (int j = 0; j < image.cols; j++)
+        {
+            hist[image.at<uchar>(i, j)]++;
+        }
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        hist[i] /= total;
+    }
+    //Calculate CDF
+    cdf.resize(256);
+    cdf[0] = hist[0];
+    for (int i = 1; i < 256; i++)
+    {
+        cdf[i] = cdf[i - 1] + hist[i];
+    }
+}
+
+//Histogram Equalization
+Mat ManualHistogramEqualization(Mat& src)
+{
+    vector<float> hist, cdf;
+    CalculateHistAndCDF(src, hist, cdf);
+    Mat output = src.clone();
+
+    uchar LUT[256];
+    for (int i = 0; i < 256; i++)
+    {
+        //LUT[i] = cvRound((cdf[i] - cdf[0]) / (1 - cdf[0]) * 255);
+        LUT[i] = static_cast<uchar>((cdf[i] - cdf[0]) / (1.0f - cdf[0]) * 255.0f);
+    }
+
+    //Apply the CDF to the original image
+    for (int i = 0; i < src.rows; ++i) {
+        for (int j = 0; j < src.cols; ++j) {
+            int pixelValue = src.at<uchar>(i, j);
+            output.at<uchar>(i, j) = LUT[pixelValue];
+        }
+    }
+
+    return output;
+}
+
+Mat ManualMachingHistogram(Mat& src, Mat& ref)
+{
+    Mat result = src.clone();
+    vector<float> hist_src, cdf_src;
+    vector<float> hist_ref, cdf_ref;
+
+    CalculateHistAndCDF(src, hist_src, cdf_src);
+    CalculateHistAndCDF(ref, hist_ref, cdf_ref);
+
+    uchar LUT[256];
+    for (int i = 0; i < 256; ++i)
+    {
+        float val = cdf_src[i];
+        uchar j = 0;
+        // Tìm mức xám bên ảnh tham chiếu sao cho CDF gần nhất với val
+        while (j < 255 && cdf_ref[j] < val) j++;
+        LUT[i] = j;
+    }
+    for (int i = 0; i < src.rows; ++i) {
+        for (int j = 0; j < src.cols; ++j) {
+            result.at<uchar>(i, j) = LUT[src.at<uchar>(i, j)];
+        }
+    }
+    return result;
+}
+
+Mat Display(Mat& image)
+{
+    int histogram[256] = { 0 };
+
+    for (int y = 0; y < image.rows; y++)
+    {
+        for (int x = 0; x < image.cols; x++)
+        {
+            unsigned char pixel_value = image.at<unsigned char>(y, x);
+            histogram[pixel_value]++;
+        }
+    }
+
+    // Tìm giá trị lớn nhất trong histogram để chuẩn hóa
+    int histMax = *max_element(histogram, histogram + 256);
+
+    int hist_w = 512; int hist_h = 400;
+    int bin_w = cvRound((double)hist_w / 256);
+
+    Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(255));
+
+    // Vẽ các dòng của histogram
+    for (int i = 1; i < 256; i++) {
+        line(histImage,
+            Point(bin_w * (i - 1), hist_h - (histogram[i - 1] * hist_h / histMax)),
+            Point(bin_w * i, hist_h - (histogram[i] * hist_h / histMax)),
+            Scalar(0), 2);
+    }
+
+    return histImage;
+}
+Mat LocalHistogramEqualization(Mat& image, int blockSize)
+{
+    Mat result = image.clone();
+
+    for (int x = 0; x < image.cols; x += blockSize)
+    {
+        for (int y = 0; y < image.rows; y += blockSize)
+        {
+            int width = min(blockSize, image.cols - x);
+            int height = min(blockSize, image.rows - y);
+            Rect roi(x,y,width,height);
+            Mat srcBlock = image(roi);
+            Mat dstBlock = result(roi);
+            equalizeHist(srcBlock, dstBlock);
+            dstBlock.copyTo(result(roi));
+        }
+    }
+    return result;
+}
---
+++ Code/TestLinearConstrast/TestLinearConstrast.cpp
@@ -0,0 +1,143 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+uchar piecewiseStretch(int r, int r1, int r2, int s1, int s2);
+
+Mat BinarySlicing(const Mat& img, int lower, int upper);
+Mat HighlightSlicing(const Mat& img, int lower, int upper);
+Mat LinearStretched(const Mat& img);
+Mat PiecewiseLinearStretched(const Mat& img, int r1, int r2, int s1, int s2);
+
+int main() 
+{
+    Mat img = imread("C:/Users/tuana/Downloads/Screenshot 2025-05-08 083637.png", IMREAD_GRAYSCALE);
+    if (img.empty())
+    {
+        cout << "Cannot open the image!";
+        return -1;
+    }
+
+    Mat stretched;
+    stretched = LinearStretched(img);
+    imshow("Linear contrast stretched", stretched);
+
+    Mat peacewise;
+    peacewise = PiecewiseLinearStretched(img, 70, 180, 30, 220);
+    imshow("Peacewise Linear contrast stretched", peacewise);
+
+    Mat binary;
+    binary = BinarySlicing(stretched, 100, 150);
+    imshow("BinarySlicing", binary);
+
+    Mat highlight;
+    highlight = HighlightSlicing(stretched, 100, 150);
+    imshow("HightlightSlicing", highlight);
+
+    // Lưu và hiển thị kết quả
+    imwrite("stretched.jpg", stretched);
+    imshow("Original Image", img);
+    
+    waitKey(0);
+    return 0;
+}
+
+Mat BinarySlicing(const Mat& img, int lower, int upper)
+{
+    Mat output = Mat::zeros(img.size(), CV_8UC1); // Ảnh nhị phân đầu ra
+
+    for (int i = 0; i < img.rows; i++)
+    {
+        for (int j = 0; j < img.cols; j++)
+        {
+            uchar pixel = img.at<uchar>(i, j);
+            if (pixel > lower && pixel < upper)
+            {
+                output.at<uchar>(i, j) = 255;
+            }
+            else
+            {
+                output.at<uchar>(i, j) = 0;
+            }
+        }
+    }
+    return output;
+}
+
+Mat LinearStretched(const Mat& img)
+{
+    int rows = img.rows;
+    int cols = img.cols;
+
+    uchar r_min = 255;
+    uchar r_max = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            uchar pixel = img.at<uchar>(i, j);
+            if (pixel < r_min) r_min = pixel;
+            if (pixel > r_max) r_max = pixel;
+        }
+    }
+    cout << "Min value: " << (int)r_min << endl;
+    cout << "Max value: " << (int)r_max << endl;
+
+    // Tạo ảnh kết quả
+    Mat stretched = Mat::zeros(rows, cols, CV_8UC1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            uchar pixel = img.at<uchar>(i, j);
+            uchar new_val = (uchar)((float)(pixel - r_min) / (r_max - r_min) * 255.0f);
+            stretched.at<uchar>(i, j) = new_val;
+        }
+    }
+
+    return stretched;
+}
+
+Mat HighlightSlicing(const Mat& img, int lower, int upper)
+{
+    Mat output = img.clone();
+
+    for (int i = 0; i < img.rows; i++)
+    {
+        for (int j = 0; j < img.cols; j++)
+        {
+            uchar pixel = img.at<uchar>(i, j);
+            if (pixel > lower && pixel < upper)
+            {
+                output.at<uchar>(i, j) = 255;
+            }
+        }
+    }
+    return output;
+}
+
+// Hàm nội suy tuyến tính theo đoạn
+uchar piecewiseStretch(int r, int r1, int r2, int s1, int s2) {
+    if (r < r1)
+        return (uchar)(s1 * r) / r1;
+    else if (r <= r2)
+        return (uchar)((s2 - s1) * (r - r1) / (r2 - r1) + s1);
+    else
+        return (uchar)((255 - s2) * (r - r2) / (255 - r2) + s2);
+}
+
+Mat PiecewiseLinearStretched(const Mat& img, int r1, int r2, int s1, int s2)
+{
+    Mat output = img.clone();
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            int r = img.at<uchar>(i, j);
+            output.at<uchar>(i, j) = piecewiseStretch(r, r1, r2, s1, s2);
+        }
+    }
+    return output;
+}
